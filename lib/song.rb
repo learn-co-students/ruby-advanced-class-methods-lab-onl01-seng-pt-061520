a9f0ee2b1d9de866234cbdb673ac9ad44a402652
Cameron Hudson <camnn3@yahoo.com>
2020-07-07 07:09:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,8 +25,37 @@ class Song
     @@all << song
   end
     
-  def self.find_by_name(song_str)
+ def self.find_by_name(song_str)
+    @@all.find{|songs| songs.name == song_str}
+    #binding.pry
+  end
+
+  def self.find_or_create_by_name(song_str)
+    if find_by_name(song_str)
+      find_by_name(song_str)
+    else
+      create_by_name(song_str)
+    end
+  end
+
+  def self.alphabetical
+    #!!!you can sort an object by an attribute
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    file_array = filename.split(/[-.]/)
+    song = self.new
+    #binding.pry
+    song.artist_name = file_array[0].strip
+    song.name = file_array[1].strip
     song
+  end
+
+  def self.create_from_filename(filename)
+    new_song = new_from_filename(filename)
+    @@all << new_song
+  end
 
   def save
     self.class.all << self
