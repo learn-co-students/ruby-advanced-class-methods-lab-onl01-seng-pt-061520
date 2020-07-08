b74b54a40f3b6c5d0a880ef39181142010b1308b
“Megan <meganschertz@gmail.com>
2020-07-08 16:29:47 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
+
+  def self.create_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.find{|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name[0]}
+  end
+
+  def self.new_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+
+    song = self.new
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_data = filename.sub!(/.mp3/,'').split(' - ')
+
+    song = self.create
+    song.artist_name = song_data[0]
+    song.name = song_data[1]
+
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
